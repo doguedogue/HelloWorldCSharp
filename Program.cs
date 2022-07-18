8382071389ec18e5013994c99ad0de0b23007963
Rafael Vilches <rvilches38@yahoo.com.mx>
2022-07-18 03:00:13 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyApp {
+    class Escuela {
+        public string nombre;
+        public string direccion;
+        public void timbrar(){
+            Console.Beep(2000,2000);
+        }
+    }
+
+    internal class Program{
+        static void Main(string[] args){
+            Console.WriteLine("Hello World!");
+            var miescuela = new Escuela();
+            miescuela.nombre = "UTM";
+            miescuela.direccion = "Oax. Mex";
+            miescuela.timbrar();
+        }
+    }
+}
